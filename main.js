13d1a2d7ab641fb7f38d29d970b256ed8e51e75e
kyle shay <kyle.shay@ymail.com>
2013-02-01 16:53:58 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,33 +1,38 @@
 javascript:(
   function(){
-    // check to see if the 'WorkMode' button has been added
-    if(!$('#settings-dropdown > .work').length) 
+    // check to see if the 'WorkMode' button has been added.
+    if(!$('#settings-dropdown>.work').length) 
     
-      // it hasn't been added, so lets 'prepend' (add it to the beginning) it to the menu
+      // it hasn't been added, so lets 'prepend' (add it to the beginning) it to the menu.
       $('#settings-dropdown').prepend(
         
         // create the new menu item
         $(document.createElement('li'))
         
-          // the option class will style it to look like all the other bottons
+          // the option class will style it to look like all the other bottons.
           .addClass('option work')
           
           // make the text say 'WorkMode' so we know what button it is.
           .text('WorkMode')
           
           // This is the handler (what to do if we click the 'WorkMode' button)
-          .live('click', function(){
+          .live('click',function(){
             
-            // This is the code that hides/shows the game-looking stuff
+            // This is the code that hides/shows the game-looking stuff.
             $('#stage-background,#dj-booth,#dj-table,#audience,'+
-            '#awesome-meter,#songboard,#header>.logo,#header>.info').toggle()
+            '#awesome-meter,#songboard,#board,#header>.logo,#header>.info').toggle();
+
+            // The rate buttongs are moved around to make things look prettier.
+            var v=$('#board').is(':visible');
+            $('#lame-button').css({left:v?9:20,top:v?6:0});
+            $('#awesome-button').css({right:v?9:20,top:v?6:0});
 
           // I call click here, so that the first time you click the bookmark
           // it will hide everything.
           }).click());
     else 
       // If the 'WorkMode' button is already added, don't add it again, 
-      // instead just 'Click' the button to hide/show everything 
-      $('#settings-dropdown > .work').click()
+      // instead just 'Click' the button to hide/show everything.
+      $('#settings-dropdown>.work').click()
   }
 )();
