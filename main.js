09e8db3c60899ec2d3b8fd170c033c47d69454a0
kyle shay <kyle.shay@ymail.com>
2013-02-01 16:11:17 -0500

    added comments
    comments

--- main.js
+++ main.js
@@ -1,1 +1,32 @@
-javascript:(function(){if(!$('#settings-dropdown > .work').length)$('#settings-dropdown').prepend($(document.createElement('li')).addClass('option work').text('WorkMode').live('click', function(){$('#stage-background,#dj-booth,#dj-table,#audience,#awesome-meter,#songboard,#header > .logo,#header > .info').toggle()}).click()); else $('#settings-dropdown > .work').click()})();
+javascript:(
+  function(){
+    // check to see if the 'WorkMode' button has been added
+    if(!$('#settings-dropdown > .work').length) 
+    
+      // it hasn't been added, so lets 'prepend' (add it to the beginning) it to the menu
+      $('#settings-dropdown').prepend(
+        
+        // create the new menu item
+        $(document.createElement('li'))
+        
+          // the option class will style it to look like all the other bottons
+          .addClass('option work')
+          
+          // make the text say 'WorkMode' so we know what button it is.
+          .text('WorkMode')
+          
+          // This is the handler (what to do if we click the 'WorkMode' button)
+          .live('click', function(){
+            // This is the code that hides/shows the game-looking stuff
+            $('#stage-background,#dj-booth,#dj-table,#audience,'+
+            '#awesome-meter,#songboard,#header>.logo,#header>.info').toggle()
+
+          // I call click here, so that the first time you click the bookmark
+          // it will hide everything.
+          }).click());
+    else 
+      // If the 'WorkMode' button is already added, don't add it again, 
+      // instead just 'Click' the button to hide/show everything 
+      $('#settings-dropdown > .work').click()
+  }
+)();
