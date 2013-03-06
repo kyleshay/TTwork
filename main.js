e20b3ed1efdda2f43ce30215eca212360f3b4cb0
kyle shay <kyle.shay@ymail.com>
2013-03-06 13:16:10 -0500

    Update main.js
    add code to hide the hanging stuff

--- main.js
+++ main.js
@@ -20,7 +20,8 @@ javascript:(
             
             // This is the code that hides/shows the game-looking stuff.
             $('#stage-background,#dj-booth,#dj-table,#audience,'+
-            '#awesome-meter,#board,#header>.logo,#header>.info').toggle();
+             '#awesome-meter,#board,#header>.logo,#header>.info'+
+             '.zoom-0 #songboard:before, .zoom-0 #songboard:after').toggle();
 
             // The rate buttongs are moved around to make things look prettier.
             var v=$('#board').is(':visible');
