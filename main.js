b834887b07700f2176adf8ae8db832ab83f01ff6
kyle shay <kyle.shay@ymail.com>
2013-04-01 12:34:34 -0300

    Update main.js
    just remove the gifs. they keep comin back otherwise :(

--- main.js
+++ main.js
@@ -20,8 +20,10 @@ javascript:(
             
             // This is the code that hides/shows the game-looking stuff.
             $('#stage-background,#dj-booth,#dj-table,#audience,'+
-            '#awesome-meter,#board,#header>.logo,#header>.info,'+
-            '#left-screen,#right-screen,#curtain').toggle();
+            '#awesome-meter,#board,#header>.logo,#header>.info').toggle();
+            
+            // Remove gifs permanently... it's ok.
+            $('#left-screen,#right-screen,#curtain').remove();
 
             // The rate buttongs are moved around to make things look prettier.
             var v=$('#board').is(':visible');
